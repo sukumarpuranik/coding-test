7fd429a7bf9a52172b93a4c291d2cf6076943369
sukumarpuranik <128472397+sukumarpuranik@users.noreply.github.com>
2023-05-11 18:25:19 +0530

    Update Shipment.java

--- src/main/java/com/monsanto/mbt/Shipment.java
+++ src/main/java/com/monsanto/mbt/Shipment.java
@@ -3,9 +3,11 @@ package com.monsanto.mbt;
 public class Shipment {
 private String color;
 private Date prductionDate;
+    
 
-public Item(String color) {
+public Item(String color,Date productionDate) {
     this.color = color;
+    this.Date=productionDate;
   }
 
 
